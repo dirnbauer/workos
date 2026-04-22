188eab46323cb6200a908bcb0bf9e754ad524b5d
Kurt Dirnbauer <dirnbauer@webconsulting.at>
2026-04-22 14:58:07 +0200

    Fix CI coding standards and mutation reporting

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,23 @@
 <?php
 
-$config = \TYPO3\CodingStandards\CsFixerConfig::create();
-$config->getFinder()
-    ->in(__DIR__)
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->files()
+    ->name('*.php')
+    ->in([
+        __DIR__ . '/Classes',
+        __DIR__ . '/Configuration',
+        __DIR__ . '/Tests',
+        __DIR__ . '/Build/phpstan/stubs',
+    ])
+    ->append([
+        __DIR__ . '/ext_emconf.php',
+        __DIR__ . '/ext_localconf.php',
+    ])
 ;
+
+$config = \TYPO3\CodingStandards\CsFixerConfig::create();
+$config->setFinder($finder);
 
 return $config;
